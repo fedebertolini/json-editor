4d18e06b889e5dff00df695fb072185f1a857fc8
Federico Bertolini <fb@hellofresh.com>
2017-05-04 20:18:59 +0200

    base redux store

---
+++ src/store/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux'
+import edited from './edited'
+import saved from './saved'
+
+export default combineReducers({
+    edited,
+    saved
+});
---
+++ src/store/reducers/saved.js
@@ -0,0 +1,3 @@
+import { Map } from 'immutable';
+
+export default (state = new Map(), action) => state;
