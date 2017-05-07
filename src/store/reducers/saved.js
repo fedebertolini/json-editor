4767be20f29b6fdf2554ea1f66c60e7ccc5acd03
Federico Bertolini <fb@hellofresh.com>
2017-05-07 21:32:52 +0200

    saved data actions and reducers

--- src/store/reducers/saved.js
+++ src/store/reducers/saved.js
@@ -1,3 +1,31 @@
-import { Map } from 'immutable';
+import { fromJS } from 'immutable';
+import {
+    SAVED_JSON_FETCH_IN_PROGRESS,
+    SAVED_JSON_FETCH_SUCCESS,
+    SAVED_JSON_FETCH_ERROR,
+} from '../constants';
 
-export default (state = new Map(), action) => state;
+const initialState = () => fromJS({
+    data: {},
+    loaded: false,
+    inProgress: false,
+    error: null,
+});
+
+export default (state = initialState(), action) => {
+    switch(action.type) {
+        case SAVED_JSON_FETCH_IN_PROGRESS:
+            return state.set('inProgress', true);
+        case SAVED_JSON_FETCH_SUCCESS:
+            return fromJS({
+                data: action.payload,
+                loaded: true,
+                inProgress: false,
+                error: null,
+            });
+        case SAVED_JSON_FETCH_ERROR:
+            return state.set('error', action.error);
+        default:
+            return state;
+    }
+};
