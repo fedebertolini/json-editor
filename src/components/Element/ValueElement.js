c1383f1fb83aac4dd3238deafff9a1ab6725d254
Federico Bertolini <fb@hellofresh.com>
2017-05-13 16:58:41 +0200

    fix printed value

--- src/components/Element/ValueElement.js
+++ src/components/Element/ValueElement.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Actions from '../Actions';
 
+const printValue = (value) => typeof value === 'string' ? `"${value}"` : `${value}`;
+
 const ValueElement = ({ data, path }) => (
     <li className="element_item">
         <span className="element_item_prop-name">
             {`${path[path.length - 1]}`}
         </span>
         <span className="element_item_value-separator">:</span>
-        <span>{data}</span>
+        <span>{printValue(data)}</span>
 
         <Actions path={path} />
     </li>
