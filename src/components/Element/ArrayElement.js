1839f7c794863da8e870cf0e184a7b020dfec8cf
Federico Bertolini <fb@hellofresh.com>
2017-05-13 21:08:34 +0200

    array toggle

--- src/components/Element/ArrayElement.js
+++ src/components/Element/ArrayElement.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Icon } from 'semantic-ui-react';
 import Element from './Element';
 import Actions from '../Actions';
 
@@ -6,27 +7,43 @@ const getPropertyName = (path) => {
     return path.length ? `${path[path.length - 1]}` : 'array';
 }
 
-const ArrayElement = ({ data, path }) => {
-    const elements = data.toArray().map((value, index) => (
-        <Element key={index} path={path.concat([index])} />
-    ));
+class ArrayElement extends Component {
 
-    return (
-        <li className="element_item">
-            <div>
-                <span className="element_item_prop-name">
-                    {getPropertyName(path)}
-                </span>
-                <span className="element_item_array-size">
-                    {`[${data.size}]`}
-                </span>
-                <Actions path={path} allowAddItem />
-            </div>
-            <ul className="element_list">
-                {elements}
-            </ul>
-        </li>
-    );
-};
+    componentWillMount() {
+        this.state = { collapse: true };
+        this.toggleCollapse = this.toggleCollapse.bind(this);
+    }
+
+    toggleCollapse() {
+        this.setState({ collapse: !this.state.collapse });
+    }
+
+    render() {
+        const { data, path } = this.props;
+        const elements = data.toArray().map((value, index) => (
+            <Element key={index} path={path.concat([index])} />
+        ));
+
+        return (
+            <li className="element_item">
+                <div>
+                    <span onClick={this.toggleCollapse} className="element_item_collapse">
+                        <Icon name={`caret ${this.state.collapse ? 'right' : 'down'}`} />
+                    </span>
+                    <span className="element_item_prop-name">
+                        {getPropertyName(path)}
+                    </span>
+                    <span className="element_item_array-size">
+                        {`[${data.size}]`}
+                    </span>
+                    <Actions path={path} allowAddItem />
+                </div>
+                <ul className="element_list">
+                    {this.state.collapse || elements}
+                </ul>
+            </li>
+        );
+    }
+}
 
 export default ArrayElement;
