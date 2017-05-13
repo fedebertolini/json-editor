cd4b6f4ccc4b228e304116f3bdbb6fea8bb8dc14
Federico Bertolini <fb@hellofresh.com>
2017-05-13 21:11:18 +0200

    object toggle

--- src/components/Element/ObjectElement.js
+++ src/components/Element/ObjectElement.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Icon } from 'semantic-ui-react';
 import Element from './Element';
 import Actions from '../Actions';
 
@@ -6,27 +7,43 @@ const getPropertyName = (path) => {
     return path.length ? `${path[path.length - 1]}` : 'array';
 }
 
-const ObjectElement = ({ data, path }) => {
-    const elements = data.keySeq().map(key => (
-        <Element key={key} path={path.concat([key])} />
-    ));
+class ObjectElement extends Component {
 
-    return (
-        <li className="element_item">
-            <div>
-                <span className="element_item_prop-name">
-                    {getPropertyName(path)}
-                </span>
-                <span className="element_item_array-size">
-                    {`{${data.size}}`}
-                </span>
-                <Actions path={path} allowAddProperty />
-            </div>
-            <ul className="element_list">
-                {elements}
-            </ul>
-        </li>
-    );
-};
+    componentWillMount() {
+        this.state = { collapse: true };
+        this.toggleCollapse = this.toggleCollapse.bind(this);
+    }
+
+    toggleCollapse() {
+        this.setState({ collapse: !this.state.collapse });
+    }
+
+    render() {
+        const { data, path } = this.props;
+        const elements = data.keySeq().map(key => (
+            <Element key={key} path={path.concat([key])} />
+        ));
+
+        return (
+            <li className="element_item">
+                <div>
+                    <span onClick={this.toggleCollapse} className="element_item_collapse">
+                        <Icon name={`caret ${this.state.collapse ? 'right' : 'down'}`} />
+                    </span>
+                    <span className="element_item_prop-name">
+                        {getPropertyName(path)}
+                    </span>
+                    <span className="element_item_array-size">
+                        {`{${data.size}}`}
+                    </span>
+                    <Actions path={path} allowAddProperty />
+                </div>
+                <ul className="element_list">
+                    {this.state.collapse || elements}
+                </ul>
+            </li>
+        );
+    }
+}
 
 export default ObjectElement;
