552f56dc6577b07d1371e268a09417d6c3f6a689
Federico Bertolini <fb@hellofresh.com>
2017-05-18 20:43:43 +0200

    dont save store in local storage because it's integration with undoable is buggy

--- src/store/index.js
+++ src/store/index.js
@@ -1,27 +1,18 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
-import { fromJS } from 'immutable';
+import { Map } from 'immutable';
 import reducers from './reducers';
-import { loadState, saveState } from '../utils/storePersistence';
 
 export const initStore = () => {
-    const isDev = process.env.NODE_ENV === 'development';
     const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-    const persistedState = fromJS((isDev && loadState()) || {});
 
     const store = createStore(
         reducers,
-        persistedState,
+        new Map(),
         composeEnhancers(
             applyMiddleware(thunk)
         ),
     );
-
-    if (isDev) {
-        store.subscribe(() => {
-            saveState(store.getState());
-        });
-    }
 
     return store;
 };
