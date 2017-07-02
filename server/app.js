783f43f9e413888022119403fcd0cd6dafa274c8
Federico Bertolini <fb@hellofresh.com>
2017-07-02 20:36:13 +0200

    move client code to root, merged package.json files to make zeit.now deployments easier

--- server/app.js
+++ server/app.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -14,14 +13,12 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use('/static', express.static(path.join(__dirname, '/../client/build/static/')));
+app.use('/static', express.static(path.join(__dirname, '/../build/static/')));
 
 app.use('/', index);
 app.use('/api', api);
--- server/routes/index.js
+++ server/routes/index.js
@@ -4,7 +4,9 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.sendFile(path.join(__dirname+'/../../client/build/index.html'));
+    console.log(__dirname);
+    console.log(path.join(__dirname+'/../../build/index.html'));
+    res.sendFile(path.join(__dirname+'/../../build/index.html'));
 });
 
 module.exports = router;
